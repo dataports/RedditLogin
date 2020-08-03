e84b64389c6d9655ad50fb53054a56592cefee18
Sophie Amin <aminsophie@gmail.com>
2020-08-03 16:46:21 -0600

    Initial Commit

---
+++ RedditLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RedditLogin
+//
+//  Created by Sophie Amin on 2020-08-03.
+//  Copyright © 2020 Sophie Amin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
